13926c0fa7e832ebff55c606bdb7e43dc3d4914c
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-09-23 22:02:46 +0600

    add phone cards and input field

---
+++ src/components/Header/Phones/Phones.jsx
@@ -0,0 +1,16 @@
+import PhonesCard from "./PhonesCard";
+
+const Phones = ({phones}) => {
+    return (
+        <div>
+            <h2 className='text-2xl text-center'>All Categories Phones</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {
+                phones?.map(phone => <PhonesCard key={phone.id} phone={phone}></PhonesCard>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Phones;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import Banner from "../../components/Header/Banner/Banner";
+import Phones from "../../components/Header/Phones/Phones";
+
 const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
+  const phones = useLoaderData();
+  console.log(phones);
+
+  return (
+    <div className="mb-10">
+      <Banner></Banner>
+      <Phones phones={phones}></Phones>
+    </div>
+  );
 };
 
 export default Home;
