428dac24039daaf386e9c5756ee7a717f7406694
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-09-24 15:50:23 +0600

    added

--- src/components/Header/Phones/PhonesCard.jsx
+++ src/components/Header/Phones/PhonesCard.jsx
@@ -1,7 +1,8 @@
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 const PhonesCard = ({ phone }) => {
-  const {id, image, phone_name, brand_name, price, rating } = phone || {};
+  const { id, image, phone_name, brand_name, price, rating } = phone || {};
   return (
     <div>
       <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
@@ -21,17 +22,17 @@ const PhonesCard = ({ phone }) => {
             </p>
           </div>
           <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
-            {rating}
+            <Ratingg initialRating={rating} readonly />
           </p>
         </div>
         <div className="p-6 pt-0">
           <Link to={`/phones/${id}`}>
-          <button
-            className="block bg-green-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-          >
-            Favorite
-          </button>
+            <button
+              className="block bg-green-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+            >
+              Favorite
+            </button>
           </Link>
         </div>
       </div>
--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useState } from "react";
+import PhoneCard from "../Phone/PhoneCard";
+import FavoriteCart from "./FavoriteCart";
+
 const Favorite = () => {
-    return (
-        <div>
-            favorite
-        </div>
-    );
+  const [favorites, setFavorites] = useState([]);
+  const [noFound, setNotfound] = useState("");
+  const [isShow, setIsShow] = useState("");
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const favoriteItems = JSON.parse(localStorage.getItem("favorites"));
+    if (favoriteItems) {
+      setFavorites(favoriteItems);
+      const total = favoriteItems.reduce((preValue, currentItem) => preValue + currentItem.price, 0)
+      setTotalPrice(total)
+    } else {
+      setNotfound("No Data Found!");
+    }
+  }, []);
+
+  // delete all the data from localstroage
+
+  const handleRemove = () =>{
+    localStorage.clear()
+    setFavorites([])
+    setNotfound("No Data Found!")
+  }
+
+  return <div>{noFound ? 
+  
+  <p className="flex justify-center items-center h-[60vh] text-5xl font-semibold">{noFound}</p> : 
+  
+    <div>
+      {
+      favorites.length > 0 && (<div>
+        <button onClick={handleRemove} className="btn bg-green-200 block mb-5 mx-auto"> Delete All Favorite</button>
+        <h2>Total Price: {totalPrice.toFixed(2)}</h2>
+      </div>)
+      
+      }
+     
+
+      <div className="grid grid-cols-2 gap-7">
+        {
+          isShow? favorites?.map(phone => (<FavoriteCart key={phone.id} phone={phone}></FavoriteCart>)) : favorites?.slice(0, 4).map(phone => (<FavoriteCart key={phone.id} phone={phone}></FavoriteCart>))
+
+        
+        } 
+      </div>
+
+      {
+        favorites.length > 2 && <button onClick={()=>setIsShow(!isShow)} className="btn bg-green-200 block mt-5 mx-auto">{isShow ? "See Less" : "See More" } </button>
+      }
+    </div>
+  
+  }
+  
+  
+  </div>;
 };
 
 export default Favorite;
